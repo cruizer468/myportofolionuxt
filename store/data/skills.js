7e2e2745ba4b51739ac0e305dd30a5b5dba4933f
cruizer468 <cruizer468@gmail.com>
2021-01-12 10:39:01 +0700

    revisi skill and project

--- store/data/skills.js
+++ store/data/skills.js
@@ -4,16 +4,16 @@ export default [
     name: 'Photoshop',
     years: '3 months',
     },
-    {
-    icon_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/160px-Figma-logo.svg.png',
-    name: 'Figma',
-    years: '1 months',
-    },
-    {
-    icon: "devicon-go-plain colored",
-    name: 'Golang',
-    years: '1 months',
-    },
+    // {
+    // icon_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/160px-Figma-logo.svg.png',
+    // name: 'Figma',
+    // years: '1 months',
+    // },
+    // {
+    // icon: "devicon-go-plain colored",
+    // name: 'Golang',
+    // years: '1 months',
+    // },
     {
     icon: "devicon-css3-plain colored",
     name: 'CSS',
@@ -27,28 +27,28 @@ export default [
     {
     icon: "devicon-laravel-plain colored",
     name: 'Laravel',
-    years: '3 months',
+    years: '1 months',
     },
     {
     icon: "devicon-sass-plain colored",
     name: 'SASS',
-    years: '2 months',
+    years: '1 months',
     },
     {
     icon: "devicon-vuejs-plain colored",
     name: 'VueJS',
-    years: '3 months',
+    years: '1 months',
     },
     {
     icon: "devicon-jquery-plain colored",
     name: 'jQUery',
     years: '2 years',
     },
-    {
-    icon: "devicon-express-original",
-    name: 'ExpressJS',
-    years: '2 months',
-    },
+    // {
+    // icon: "devicon-express-original",
+    // name: 'ExpressJS',
+    // years: '2 months',
+    // },
     {
     icon: "devicon-python-plain colored",
     name: 'Python',
@@ -57,7 +57,7 @@ export default [
     {
     icon: "devicon-nodejs-plain colored",
     name: 'NodeJS',
-    years: '6 months',
+    years: '2 months',
     },
     {
     icon: "devicon-wordpress-plain colored",
@@ -74,9 +74,9 @@ export default [
     name: 'PostgreSQL',
     years: '2 years',
     },
-    { 
-    icon: "devicon-mongodb-plain colored",
-    name: 'MongoDB',
-    years: '2 months',
-    }
+    // { 
+    // icon: "devicon-mongodb-plain colored",
+    // name: 'MongoDB',
+    // years: '2 months',
+    // }
 ]
